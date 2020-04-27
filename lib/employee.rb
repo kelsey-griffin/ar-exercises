ca7b293aee54d5f284aa9658a6dab87c390e47cd
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-27 22:52:52 +0000

    add validation

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  validates_presence_of :store_id, :first_name, :last_name
+  validates :hourly_rate, :numericality => {:greater_than_or_equal_to => 40, :less_than_or_equal_to => 200} 
+
 end
